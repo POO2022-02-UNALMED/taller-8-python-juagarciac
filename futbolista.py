b9c09d8a2d48c7d722c6c2aaadbf368baeebf476
juagarciac <111471638+juagarciac@users.noreply.github.com>
2022-10-16 18:04:22 -0500

    j

---
+++ futbolista.py
@@ -0,0 +1,31 @@
+import string
+from deportista import Deportista
+from persona import Persona
+class Futbolista(Persona,Deportista):
+    listaFutbolistas=[]
+    def __init__(self,nombre="",edad=0,altura="0",sexo="",deporte="Futbol", añosPracticando=0,goles=0,tarjetas=0,pierna="derecha"):
+        Persona.__init__(nombre,edad,altura,sexo)
+        Deportista.__init__(deporte,añosPracticando)
+        self._golesMarcados=goles
+        self._tarjetasRojas=tarjetas
+        self._piernaHabil=pierna
+        self.listaFutbolistas.append(self)
+    def getGolesMarcados(self):
+        return self._golesMarcados
+    def setGolesMarcados(self,goles):
+        self._golesMarcados=goles
+    def getTarjetasRojas(self):
+        return self._tarjetasRojas
+    def setTarjetasRojas(self,tarjetas):
+        self._tarjetasRojas=tarjetas
+    def getPiernaHabil(self):
+        return self._piernaHabil
+    def setPiernaHabil(self,pierna):
+        self._piernaHabil=pierna
+    @classmethod
+    def getListaFutbolistas(cls):
+        return cls.listaFutbolistas
+    def setListaFutbolistas(cls,lista):
+        cls.listaFutbolistas=lista
+    def __str__(self):
+        return "Mi nombre es "+Persona._nombre+" soy profesional en el deporte "+Deportista._deporte+" Tengo "+str(Persona._edad)+" años de edad y llevo "+str(Deportista._añosPracticando)+" años en el deporte"
